48b2f915edf27f419c40adbd5f157a720346898d
proDOOMman <prodoomman@gmail.com>
2011-05-14 17:04:42 +0400

    Scripts update

--- gmail.sh
+++ gmail.sh
@@ -1,11 +1,21 @@
 #!/bin/sh
-#dbus-send --system /default com.lab126.wifid.cmConnected && sleep 10
+
+#settings:
 MPLAYER=/mnt/us/fbKindle/mplayer/mplayer # mplayer binary (or any other player)
-USER=username # GMail account nickname
+USER=user # GMail account nickname
 PASS=password # GMail account password
 SOUND1=/mnt/us/KDE-Im-Phone-Ring.ogg # New mail sound
 SOUND2=/mnt/us/KDE-Sys-App-Message.ogg # No new mail sound
-COUNT=`curl -su $USER:$PASS https://mail.google.com/mail/feed/atom || echo "<fullcount>0</fullcount>"`
+##########
+
+#check wlan:
+if [ `lipc-get-prop com.lab126.wan powerState` -ne 1 ]; then
+/mnt/us/notify/notify-send "Please, enable WIFI/3G first"
+exit;
+fi
+
+#downloading and parsing atom:
+COUNT=`curl -su $USER:$PASS https://mail.google.com/mail/feed/atom || echo "<fullcount>unknown number of</fullcount>"`
 COUNT=`echo "$COUNT" | grep "<fullcount>" | sed -e s/"<fullcount>"// -e s/"<\/fullcount>"//`
 echo $COUNT
 if [ "$COUNT" != "0" ]; then
@@ -18,4 +28,3 @@ $MPLAYER "$SOUND1" & /mnt/us/notify/notify-send "There is $COUNT new $WORD";
 else
 $MPLAYER "$SOUND2" & /mnt/us/notify/notify-send "No new mails =(";
 fi
-#dbus-send --system /default com.lab126.wifid.cmNotconnected
